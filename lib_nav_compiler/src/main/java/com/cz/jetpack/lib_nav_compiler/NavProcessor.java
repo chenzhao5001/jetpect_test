e4a37031ffb84fbae09445a027b314083a86f51d
chenzhao <chenzhao5001@163.com>
2022-12-06 15:07:19 +0800

    注解处理器

--- lib_nav_compiler/src/main/java/com/cz/jetpack/lib_nav_compiler/NavProcessor.java
+++ lib_nav_compiler/src/main/java/com/cz/jetpack/lib_nav_compiler/NavProcessor.java
@@ -1,12 +1,16 @@
 package com.cz.jetpack.lib_nav_compiler;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.cz.jetpack.lib_nav_annotation.ActivityDestination;
 import com.cz.jetpack.lib_nav_annotation.FragmentDestination;
 import com.google.auto.service.AutoService;
 
-import org.graalvm.compiler.serviceprovider.ServiceProvider;
-
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Set;
@@ -21,6 +25,9 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 
 
 @AutoService(Process.class)
@@ -28,8 +35,10 @@ import javax.lang.model.element.TypeElement;
 @SupportedAnnotationTypes({"com.cz.jetpack.lib_nav_annotation.ActivityDestination",
         "com.cz.jetpack.lib_nav_annotation.FragmentDestination"})
 public class NavProcessor extends AbstractProcessor {
-    Messager messager;
-    Filer filer;
+    private Messager messager;
+    private Filer filer;
+    private static final String OUT_FILE_NAME = "destination.json";
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
@@ -42,14 +51,100 @@ public class NavProcessor extends AbstractProcessor {
         Set<? extends Element> fragmentElements = roundEnvironment.getElementsAnnotatedWith(FragmentDestination.class);
         Set<? extends Element> activityElements = roundEnvironment.getElementsAnnotatedWith(ActivityDestination.class);
 
-        if(!fragmentElements.isEmpty() || !activityElements.isEmpty()) {
+        if (!fragmentElements.isEmpty() || !activityElements.isEmpty()) {
 
             HashMap<String, JSONObject> destMap = new HashMap<>();
-            handleDestination(fragmentElements,FragmentDestination.class,destMap);
+            handleDestination(fragmentElements, FragmentDestination.class, destMap);
+            handleDestination(activityElements, ActivityDestination.class, destMap);
+
+            // app // src // main // assets
+
+            FileObject fileObject = null;
+            FileOutputStream fos = null;
+            OutputStreamWriter writer = null;
+            try {
+                fileObject = filer.createResource(StandardLocation.CLASS_OUTPUT,"",OUT_FILE_NAME);
+                String resourcePath = fileObject.toUri().getPath();
+                messager.printMessage(Diagnostic.Kind.NOTE,"resourcePath: " + resourcePath);
+                String appPath = resourcePath.substring(0,resourcePath.indexOf("app") + 4);
+                String assetsPath = appPath + "src/main/assets";
+
+                File file = new File(assetsPath);
+                if(!file.exists()) {
+                    file.mkdir();
+                }
+                File outPutFile = new File(file,OUT_FILE_NAME);
+                if(outPutFile.exists()) {
+                    outPutFile.delete();
+                }
+                outPutFile.createNewFile();
+
+                String content = JSON.toJSONString(destMap);
+                fos = new FileOutputStream(outPutFile);
+                writer = new OutputStreamWriter(fos,"UTF-8");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
         return true;
     }
 
-    private void handleDestination(Set<? extends Element> fragmentElements, Class<? extends Annotation> AnnotationClass, HashMap<String, JSONObject> destMap) {
+    private void handleDestination(Set<? extends Element> elements, Class<? extends Annotation> annotationClass, HashMap<String, JSONObject> destMap) {
+
+        for (Element element : elements) {
+            TypeElement typeElement = (TypeElement) element;
+            String pageUrl = null;
+            String clazzName = typeElement.getQualifiedName().toString();
+            int id = Math.abs(clazzName.hashCode());
+            boolean needLogin = false;
+            boolean asStarter = false;
+            boolean isFragment = false;
+
+
+            Annotation annotation = typeElement.getAnnotation(annotationClass);
+            if (annotation instanceof FragmentDestination) {
+                FragmentDestination dest = (FragmentDestination) annotation;
+                pageUrl = dest.pageUrl();
+                asStarter = dest.asStarter();
+                needLogin = dest.needLogin();
+                isFragment = true;
+            } else if(annotation instanceof ActivityDestination) {
+                ActivityDestination dest = (ActivityDestination) annotation;
+                pageUrl = dest.pageUrl();
+                asStarter = dest.asStarter();
+                needLogin = dest.needLogin();
+                isFragment = false;
+            }
+
+            if(destMap.containsKey(pageUrl)) {
+                messager.printMessage(Diagnostic.Kind.ERROR,"不同的页面不允许使用相同的pageUrl: " + clazzName);
+            } else {
+                JSONObject object = new JSONObject();
+                object.put("id",id);
+                object.put("needLogin",needLogin);
+                object.put("asStarter",asStarter);
+                object.put("pageUrl",pageUrl);
+                object.put("clazzName",clazzName);
+                object.put("isFragment",isFragment);
+            }
+
+
+        }
     }
 }
